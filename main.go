c274cf35ca80912f2206c0eff515c2e01f612151
Matias Sierra <matisierra.develop@gmail.com>
2023-07-22 21:57:31 -0300

    add fiber return data

--- main.go
+++ main.go
@@ -9,20 +9,20 @@ import (
 )
 
 func main() {
-	app := fiber.New()
-	//Midleware
-	app.Use(cors.New())
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
+	app := fiber.New()
+	//Midleware
+	app.Use(cors.New())
+
 	//Server files
 	app.Static("/", "./client/dist")
 
 	app.Get("/user", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
+		return c.JSON(&fiber.Map{
 			"name": "Grame",
 			"age":  20,
 		})
