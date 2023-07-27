65681c5cdbd75f2d2294321b128dc89cdb757536
Matias Sierra <matisierra.develop@gmail.com>
2023-07-26 23:01:17 -0300

    Add vars for string connection for postgres

--- pkg/config/db.go
+++ pkg/config/db.go
@@ -2,17 +2,33 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-var STRINGPQ = "user=pqgotest dbname=pqgotest sslmode=verify-full"
 var DB *sql.DB
+var (
+	POSTGRES_HOST     string = "localhost"
+	POSTGRES_PORT     int    = 5432
+	POSTGRES_USER     string = "postgres"
+	POSTGRES_PASSWORD string = "rootroot"
+	POSTGRES_DBNAME   string = "pqreact"
+)
 
 func DBconfig() (*sql.DB, error) {
+	stringconn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		POSTGRES_HOST,
+		POSTGRES_PORT,
+		POSTGRES_USER,
+		POSTGRES_PASSWORD,
+		POSTGRES_DBNAME,
+	)
+
 	var err error
-	DB, err = sql.Open("postgres", STRINGPQ)
+	DB, err = sql.Open("postgres", stringconn)
 
 	if err != nil {
 		log.Fatal(err)
