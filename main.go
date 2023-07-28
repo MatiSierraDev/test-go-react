ee2a30fddad574172021486287b3b8d412d35ccc
Matias Sierra <matisierra.develop@gmail.com>
2023-07-28 02:30:57 -0300

    Add(testing) request GET '/test' for users

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	_ "fmt"
+	"log"
 	"os"
 
+	"github.com/MatiSierraDev/6-react-fiber/pkg/config"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -20,6 +23,67 @@ func main() {
 
 	//Server files
 	app.Static("/", "./client/dist")
+
+	config.DBconfig()
+
+	type Task struct {
+		Id          uuid.UUID `json:"id"`
+		Title       string    `json:"title"`
+		Description string    `json:"description"`
+	}
+
+	type user struct {
+		Id    uuid.UUID `json:"id"`
+		Name  string    `json:"name"`
+		Email string    `json:"email"`
+		Tasks []Task    `json:"tasks"`
+	}
+
+	app.Get("/test", func(c *fiber.Ctx) error {
+
+		db, err := config.DBconfig()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// SELECT users.user_id, user_name, user_email, task_id,
+		// task_title, task_description
+		// FROM users
+		// INNER JOIN tasks
+		// on users.user_id = tasks.user_id;
+		query := `SELECT users.user_id, user_name, user_email, task_id, task_title, task_description
+							FROM users
+							INNER JOIN tasks
+							on users.user_id = tasks.user_id;`
+
+		// query = `SELECT * FROM users`
+
+		rows, err := db.Query(query)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer rows.Close()
+
+		var users []user
+		// var tasks Task
+
+		for rows.Next() {
+			var user user
+			var task Task
+			err := rows.Scan(&user.Id, &user.Name, &user.Email, &task.Id, &task.Title, &task.Description)
+			if err != nil {
+				log.Fatal(err)
+			}
+			user.Tasks = append(user.Tasks, task)
+			users = append(users, user)
+
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"data": users})
+	})
 
 	app.Get("/user", func(c *fiber.Ctx) error {
 		return c.JSON(&fiber.Map{
