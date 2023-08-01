3ec07f246d39977267a5d4cb290f4a419ab6f688
Matias Sierra <matisierra.develop@gmail.com>
2023-08-01 17:39:56 -0300

    Add(POST) hardcode post data for postgres database

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	_ "fmt"
+	"log"
 	"os"
 
 	"github.com/MatiSierraDev/6-react-fiber/pkg/config"
 	"github.com/MatiSierraDev/6-react-fiber/pkg/controllers"
+	"github.com/MatiSierraDev/6-react-fiber/pkg/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	_ "github.com/google/uuid"
@@ -29,6 +31,40 @@ func main() {
 	apiRoute := controllers.Api{}
 
 	app.Get("/test", apiRoute.GetAlls)
+	app.Post("/test", func(ctx *fiber.Ctx) error {
+
+		DB, err := config.DBconfig()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var newUser models.User
+
+		newUser.Name = "BlacKI"
+		newUser.Email = "BlacKI@gmail.com"
+
+		query := `INSERT INTO users(user_name, user_email)
+						VALUES ($1,$2) RETURNING *`
+
+		// result, err := DB.Exec(query, newUser.Name, newUser.Email)
+
+		//guardo(&scan) los datos que me devuelve la DB en mi variable
+		err = DB.QueryRow(query, newUser.Name, newUser.Email).Scan(&newUser.Id, &newUser.Name, &newUser.Email)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// data, err := result.RowsAffected()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return ctx.JSON(&newUser)
+
+	})
 
 	app.Get("/user", func(c *fiber.Ctx) error {
 		return c.JSON(&fiber.Map{
