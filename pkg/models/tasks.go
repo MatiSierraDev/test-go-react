ba041013695d82244b9642b73e56bf3c988dd4d0
Matias Sierra <matisierra.develop@gmail.com>
2023-08-08 13:17:46 -0300

    Change models zero value nil for accepted db

--- pkg/models/tasks.go
+++ pkg/models/tasks.go
@@ -1,9 +1,14 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
 
 type Task struct {
-	Id          uuid.UUID `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
+	Id *uuid.UUID `json:"id"`
+	// Title       *string    `json:"title"`
+	Title       sql.NullString `json:"title"`
+	Description sql.NullString `json:"description"`
 }
