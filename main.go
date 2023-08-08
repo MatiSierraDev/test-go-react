33be7f8f6008fecc60bb846f8feba221394bac18
Matias Sierra <matisierra.develop@gmail.com>
2023-08-08 19:39:30 -0300

    Refactor POST function main moved to controllers

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	_ "fmt"
-	"log"
 	"os"
 
 	"github.com/MatiSierraDev/6-react-fiber/pkg/config"
 	"github.com/MatiSierraDev/6-react-fiber/pkg/controllers"
-	"github.com/MatiSierraDev/6-react-fiber/pkg/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	_ "github.com/google/uuid"
@@ -30,41 +28,8 @@ func main() {
 
 	apiRoute := controllers.Api{}
 
-	app.Get("/test", apiRoute.GetAlls)
-	app.Post("/test", func(ctx *fiber.Ctx) error {
-
-		DB, err := config.DBconfig()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var newUser models.User
-
-		newUser.Name = "Chinchu"
-		newUser.Email = "Chinchu@gmail.com"
-
-		query := `INSERT INTO users(user_name, user_email)
-						VALUES ($1,$2) RETURNING *`
-
-		// result, err := DB.Exec(query, newUser.Name, newUser.Email)
-
-		//guardo(&scan) los datos que me devuelve la DB en mi variable
-		err = DB.QueryRow(query, newUser.Name, newUser.Email).Scan(&newUser.Id, &newUser.Name, &newUser.Email)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// data, err := result.RowsAffected()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		return ctx.JSON(&newUser)
-
-	})
+	app.Get("/api/users", apiRoute.GetAlls)
+	app.Post("/api/users", apiRoute.CreateUser)
 
 	app.Get("/user", func(c *fiber.Ctx) error {
 		return c.JSON(&fiber.Map{
